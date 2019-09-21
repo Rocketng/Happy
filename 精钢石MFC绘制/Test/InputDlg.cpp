a3667b7b48a4ad99a35d2558cc80cb57093c14cc
Fengyong Peng <2422380414@qq.com>
2019-09-21 12:09:27 +0800

    精钢石提交

---
+++ 精钢石MFC绘制/Test/InputDlg.cpp
@@ -0,0 +1,59 @@
+// InputDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "Test.h"
+#include "InputDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CInputDlg dialog
+
+
+CInputDlg::CInputDlg(CWnd* pParent /*=NULL*/)
+	: CDialog(CInputDlg::IDD, pParent)
+{
+	//{{AFX_DATA_INIT(CInputDlg)
+	m_n = 0;
+	m_r = 0.0;
+	//}}AFX_DATA_INIT
+}
+
+
+void CInputDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CInputDlg)
+	DDX_Text(pDX, IDC_EDIT1, m_n);
+	DDV_MinMaxInt(pDX, m_n, 5, 50);
+	DDX_Text(pDX, IDC_EDIT2, m_r);
+	DDV_MinMaxDouble(pDX, m_r, 200., 300.);
+	//}}AFX_DATA_MAP
+}
+
+
+BEGIN_MESSAGE_MAP(CInputDlg, CDialog)
+	//{{AFX_MSG_MAP(CInputDlg)
+	ON_WM_SHOWWINDOW()
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CInputDlg message handlers
+
+void CInputDlg::OnShowWindow(BOOL bShow, UINT nStatus) 
+{
+	CDialog::OnShowWindow(bShow, nStatus);
+	
+	// TODO: Add your message handler code here
+
+	//新增代码部分,设置输入框1的内容为选中状态
+	GetDlgItem(IDC_EDIT1)->SetFocus();
+	((CEdit *)GetDlgItem(IDC_EDIT1))->SetSel(0,-1);
+	
+}
